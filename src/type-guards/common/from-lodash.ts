4caaf5569d1ffcc13af16c1b2725a7444ca53334
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-10-10 12:00:56 -0300

    Common type guards

---
+++ src/type-guards/common/from-lodash.ts
@@ -0,0 +1,40 @@
+import {
+  isArray,
+  isBoolean,
+  isEmpty,
+  isEqual as isDeepEqual,
+  isFunction,
+  isInteger as lodashIsInteger,
+  isNil,
+  isNull,
+  isNumber,
+  isString,
+  isUndefined,
+} from 'lodash-es'
+import { PredicateFn } from '../predicate-fn'
+
+export { isArray, isBoolean, isEmpty, isDeepEqual, isFunction, isNil, isNull, isNumber, isString, isUndefined }
+
+/**
+ * Checks if `value` is an integer.
+ *
+ * **Note:** This method is based on [`Number.isInteger`](https://mdn.io/Number/isInteger).
+ *
+ * @category Lang
+ * @param value The value to check.
+ * @returns Returns `true` if `value` is an integer, else `false`.
+ * @example
+ *
+ * _.isInteger(3);
+ * // => true
+ *
+ * _.isInteger(Number.MIN_VALUE);
+ * // => false
+ *
+ * _.isInteger(Infinity);
+ * // => false
+ *
+ * _.isInteger('3');
+ * // => false
+ */
+export const isInteger = lodashIsInteger as PredicateFn<number>
