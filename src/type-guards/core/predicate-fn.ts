0b18f53091b96074a5b377f2abea12502622d0c7
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-10-10 14:11:55 -0300

    AsyncPredicateFn

--- src/type-guards/core/predicate-fn.ts
+++ src/type-guards/core/predicate-fn.ts
@@ -12,9 +12,20 @@
 export type PredicateFn<T = any> = (v: unknown) => v is T
 
 /**
+ * An async predicate function that takes a single value and returns a {@link PredicateFn} wrapped
+ * in a {@link Promise} that serves as a type guard for a given type T
+ */
+export type AsyncPredicateFn<T = any> = (value: unknown) => Promise<PredicateFn<T>>
+
+/**
  * A predicate function that accepts any number of parameters
  */
 export type UnguardedPredicateFn<Params extends Array<any> = Array<any>> = (...args: Params) => boolean
+
+/**
+ * An async predicate function that accepts any number of parameters
+ */
+export type AsyncUnguardedPredicateFn<Params extends Array<any> = Array<any>> = (...args: Params) => Promise<boolean>
 
 /**
  *  Unpacks the type T from a {@link PredicateFn}
