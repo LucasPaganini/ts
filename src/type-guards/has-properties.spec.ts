c038ae3ea75e3ed2e464d0c0636c2bc158df45ac
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-10-10 11:54:03 -0300

    makeHasProperties

---
+++ src/type-guards/has-properties.spec.ts
@@ -0,0 +1,47 @@
+/*************************************************************************
+ * Copyright (c) 2021–present Lucas Paganini. All rights reserved.
+ * Contact: <me@lucaspaganini.com>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE.md file for details.
+ **************************************************************************/
+
+import { expectTypeOf } from 'expect-type'
+import { makeHasProperties } from './has-properties'
+
+describe(makeHasProperties.name, () => {
+  it('returns a predicate function that checks if value has the given properties', () => {
+    // Single property
+    const has_A = makeHasProperties(['a'])
+    expect(has_A({ a: 1, b: 2 })).toBe(true)
+    expect(has_A({ a: 1 })).toBe(true)
+    expect(has_A({})).toBe(false)
+    expect(has_A({ b: 2 })).toBe(false)
+
+    // Multiple properties
+    const has_A_B = makeHasProperties(['a', 'b'])
+    expect(has_A_B({ a: 1, b: 2 })).toBe(true)
+    expect(has_A_B({ a: 1 })).toBe(false)
+
+    // Multiple properties w/ non-literal array argument
+    const props = ['a', 'b']
+    const has_A_B_arr = makeHasProperties(props)
+    expect(has_A_B_arr({ a: 1, b: 2 })).toBe(true)
+    expect(has_A_B_arr({ a: 1 })).toBe(false)
+
+    // Multiple properties w/ const array
+    const roProps = ['a', 'b'] as const
+    const has_A_B_ro_arr = makeHasProperties(roProps)
+    expect(has_A_B_ro_arr({ a: 1, b: 2 })).toBe(true)
+    expect(has_A_B_ro_arr({ a: 1 })).toBe(false)
+  })
+
+  it('has the correct types', () => {
+    const hasPropX = makeHasProperties(['x'])
+
+    expectTypeOf(makeHasProperties).toEqualTypeOf<
+      <Property extends string>(properties: ReadonlyArray<Property>) => (v: unknown) => v is Record<Property, unknown>
+    >()
+    expectTypeOf(hasPropX).toEqualTypeOf<(v: unknown) => v is Record<'x', unknown>>()
+  })
+})
---
+++ src/type-guards/has-properties.ts
@@ -0,0 +1,47 @@
+/*************************************************************************
+ * Copyright (c) 2021–present Lucas Paganini. All rights reserved.
+ * Contact: <me@lucaspaganini.com>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE.md file for details.
+ **************************************************************************/
+
+import { assertHasProperties } from '../assertion-guards/assert-has-properties'
+import { PredicateFn } from './predicate-fn'
+
+/**
+ * Creates a {@link PredicateFn} that checks if the given value has the given
+ * properties.
+ *
+ * @example
+ * ```ts
+ * let foo: unknown = someUnknownObject;
+ *
+ * // The following expressions are equivalent:
+ * const hasPropA = makeHasProperties(['a']);
+ * const hasPropA = (v: unknown): v is Record<'a', unknown> =>
+ * 	 typeof v === 'object' &&
+ * 	 v !== null &&
+ * 	 'a' in v;
+ *
+ * // Usage
+ * foo.a // <- Compilation error
+ * if (hasPropA(foo)) {
+ *   foo.a // <- foo: { a: unknown }
+ * }
+ * ```
+ *
+ * @param properties The properties that we'll check if the given value has
+ */
+export const makeHasProperties = <Property extends string>(
+  properties: ReadonlyArray<Property>,
+): PredicateFn<Record<Property, unknown>> => {
+  return (value: unknown): value is Record<Property, unknown> => {
+    try {
+      assertHasProperties(properties, value)
+      return true
+    } catch {
+      return false
+    }
+  }
+}
