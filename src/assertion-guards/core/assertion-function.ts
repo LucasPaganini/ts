14ff2e6e50bc37dc7da941e94612babdc7e7b64c
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-10-11 09:05:54 -0300

    Support AssertionFunctions with up to 10 arguments

--- src/assertion-guards/core/assertion-function.ts
+++ src/assertion-guards/core/assertion-function.ts
@@ -6,6 +6,17 @@
  * MIT license. See the LICENSE.md file for details.
  **************************************************************************/
 
-export type AssertionFunction<T = any> = (v: unknown) => asserts v is T
+export type AssertionFunction<T = any> = {
+  (a: any, b: any, c: any, d: any, e: any, f: any, g: any, h: any, i: any, j: any, ...args: Array<any>): asserts j is T
+  (a: any, b: any, c: any, d: any, e: any, f: any, g: any, h: any, i: any, ...args: Array<any>): asserts i is T
+  (a: any, b: any, c: any, d: any, e: any, f: any, g: any, h: any, ...args: Array<any>): asserts h is T
+  (a: any, b: any, c: any, d: any, e: any, f: any, g: any, ...args: Array<any>): asserts g is T
+  (a: any, b: any, c: any, d: any, e: any, f: any, ...args: Array<any>): asserts f is T
+  (a: any, b: any, c: any, d: any, e: any, ...args: Array<any>): asserts e is T
+  (a: any, b: any, c: any, d: any, ...args: Array<any>): asserts d is T
+  (a: any, b: any, c: any, ...args: Array<any>): asserts c is T
+  (a: any, b: any, ...args: Array<any>): asserts b is T
+  (a: any, ...args: Array<any>): asserts a is T
+}
 
 export type UnpackedAssertionFunction<F extends AssertionFunction> = F extends AssertionFunction<infer T> ? T : never
