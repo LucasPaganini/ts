07052ad2059ec6677b76d536a0fc1be7b9630fd8
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-10-12 07:09:07 -0300

    Export modules on the entry file

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,7 @@
  * This software may be modified and distributed under the terms of the
  * MIT license. See the LICENSE.md file for details.
  **************************************************************************/
+
+export * from './assertion-guards'
+export * from './type-guards'
+export * from './types'
